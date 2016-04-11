87221ec7384b697e66db2a4c70d93ef74fd448a6
Stephen <sguillory63@gmail.com>
2016-04-11 14:14:59 -0500

    integrated Shave inventory and central hub

--- app/src/main/java/com/mysotd/android/myshaveoftheday/SingleFragmentActivity.java
+++ app/src/main/java/com/mysotd/android/myshaveoftheday/SingleFragmentActivity.java
@@ -6,22 +6,24 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 
 /**
- * Created by Adam on 3/13/2016.
+ * Created by Stephen on 4/3/2016.
  */
-public abstract class SingleFragmentActivity extends FragmentActivity {
 
+public abstract class SingleFragmentActivity extends FragmentActivity
+{
     protected abstract Fragment createFragment();
 
-    //Creates the activity and creates its view, which is just an empty Frame Layout
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
 
         FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container); //fragment_container is in content_item layout
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
-        if(fragment == null){
+        if(fragment == null)
+        {
             fragment = createFragment();
             fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
         }
